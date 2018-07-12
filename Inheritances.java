a1d04899cb0d43d7c533d209011cb2dd732516ea
pramanram <41136876+pramanram@users.noreply.github.com>
2018-07-12 10:07:24 +0530

    Add files via upload

---
+++ Inheritances.java
@@ -0,0 +1,20 @@
+package inheritanses1;
+ class Ram{
+	 private void writer(){
+		 System.out.println("Good Writer");
+	 }
+	 public void speaker(){
+		 System.out.println("Good Speaker");
+	 }
+	 protected void Dancing(){
+		 System.out.println("dancing");
+	 }
+	
+}
+public class Inheritances {
+
+	public static void main(String[] args) {
+
+	}
+
+}
