20fa78b25acd8daadc6038bc65eed0162b021dd0
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-07-28 15:37:36 +0530

    Add files via upload

---
+++ IntervalsEvenNumbers.java
@@ -0,0 +1,23 @@
+package guvi1;
+import java.util.Scanner;
+public class IntervalsEvenNumbers {
+		public static void main(String args[]){
+		int n;
+		int q;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		q=in.nextInt();
+		if(n<=100000){
+			for(int i=n+1;i<q;i++){
+				if(i%2==0){
+					System.out.println(i);
+				}
+			}
+			
+		}
+		
+		}
+
+	}
+
+
