3e836dd4cb7e4e1cf6031d5619e40713aef52b28
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-07-28 11:59:52 +0530

    Add files via upload

---
+++ CountIntegers.java
@@ -0,0 +1,11 @@
+package guvi1;
+import java.util.*;
+public class CountIntegers {
+	public static void main(String args[]){
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		String s=Integer.toString(n);
+		System.out.println(s.length());
+	}
+}
