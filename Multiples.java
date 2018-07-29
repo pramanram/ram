7be8d2531e25c06273e6203cb4473c629f28fcaa
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-07-29 12:00:24 +0530

    Add files via upload

---
+++ Multiples.java
@@ -0,0 +1,15 @@
+package guvi1;
+import java.util.*;
+public class Multiples {
+	public static void main(String[] args) {
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		for(int i=1;i<=5;i++){
+			System.out.println(n*i);
+			
+		}
+
+	}
+
+}
