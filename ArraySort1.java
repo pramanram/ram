dee4ebbe99f58c69619362269fa3734929f5acb9
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-08-17 12:24:05 +0530

    Add files via upload

---
+++ ArraySort1.java
@@ -0,0 +1,17 @@
+package guvi1;
+import java.util.*;
+public class ArraySort1 {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int[] a=new int[n];
+		int l=a.length;
+		for(int i=0;i<n;i++){
+			a[i]=in.nextInt();
+			}
+		Arrays.sort(a);
+		for(int b:a){
+			System.out.print(b+" ");
+		}
+	}
+}
